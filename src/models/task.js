d94e2c5754ba333b086cbb2d8b1316daaf03b6e2
Colin454 <blackcolin138@gmail.com>
2025-09-15 17:15:06 +0700

    task

---
+++ src/models/task.js
@@ -0,0 +1,13 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Task = sequelize.define('Task', {
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  title: { type: DataTypes.STRING, allowNull: false },
+  status: { 
+    type: DataTypes.ENUM('pending', 'in-progress', 'done'),
+    defaultValue: 'pending'
+  }
+});
+
+module.exports = Task;
