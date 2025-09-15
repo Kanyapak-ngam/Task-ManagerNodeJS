f9bc4b93ef84adb2bbcefade4128e393974939b9
Colin454 <blackcolin138@gmail.com>
2025-09-15 17:23:43 +0700

    Update index.js

--- task-manager/backend/src/models/index.js
+++ task-manager/backend/src/models/index.js
@@ -2,7 +2,7 @@ const sequelize = require('../config/database');
 const User = require('./user');
 const Task = require('./task');
 
-// ความสัมพันธ์: 1 User มีหลาย Task
+// ความสัมพันธ์ 1 User มีหลาย Task
 User.hasMany(Task, { foreignKey: 'userId', as: 'tasks' });
 Task.belongsTo(User, { foreignKey: 'userId', as: 'user' });
 
