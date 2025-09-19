57514e84d30ea43490a9f8670f8c8557d720e6ff
Colin454 <blackcolin138@gmail.com>
2025-09-20 04:21:19 +0700

    test

--- task-manager/backend/src/models/index.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require('../config/database');
-const User = require('./user');
-const Task = require('./task');
-
-// ความสัมพันธ์
-User.hasMany(Task, { foreignKey: 'userId', as: 'tasks' });
-Task.belongsTo(User, { foreignKey: 'userId', as: 'user' });
-
-module.exports = { sequelize, User, Task };
